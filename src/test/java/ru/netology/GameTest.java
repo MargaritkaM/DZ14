3f6e98105b53a098016a95ec66b302a31fc708f0
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-08-22 18:32:12 +0300

    Add registration to tests

--- src/test/java/ru/netology/GameTest.java
+++ src/test/java/ru/netology/GameTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
@@ -10,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
+    Game repo = new Game();
+    ArrayList<Player> player = new ArrayList<>();
 
     Player player1 = new Player(4, "Anya", 500);
     Player player2 = new Player(1, "Alina", 300);
@@ -17,24 +20,66 @@ public class GameTest {
     Player player4 = new Player(3, "Nastya", 800);
     Player player5 = new Player(5, "Kurva", 500);
 
-    Game repo = new Game();
-    ArrayList<Player> player = new ArrayList<>();
+
+    @BeforeEach
+    public void setup() {
+        repo.register(player1);
+        repo.register(player2);
+        repo.register(player3);
+        repo.register(player4);
+        repo.register(player5);
+    }
 
     @Test
     public void equalValues() throws NotRegisteredException {
 
-
-        Player player1 = new Player(4, "Anya", 500);
-        Player player2 = new Player(1, "Alina", 300);
-        Player player3 = new Player(2, "Sveta", 100);
-        Player player4 = new Player(3, "Nastya", 800);
-        Player player5 = new Player(5, "Kurva", 500);
-
-
-        int expected = 2;
-        int actual = repo.round("Anya", "Nastya");
+        int expected = 0;
+        int actual = repo.round("Anya", "Kurva");
 
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void firstStronger() throws NotRegisteredException {
+
+
+        int expected = 1;
+        int actual = repo.round("Anya", "Alina");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondStronger() throws NotRegisteredException {
+
+
+        int expected = 2;
+        int actual = repo.round("Sveta", "Alina");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void firstNotFound() throws NotRegisteredException {
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            repo.round("Margarita", "Alina");
+        });
+    }
+
+    @Test
+    public void secondNotFound() throws NotRegisteredException {
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            repo.round("Alina", "Margarita");
+        });
+    }
+
+    @Test
+    public void twoNotFound() throws NotRegisteredException {
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            repo.round("Annet", "Margarita");
+        });
+    }
 }
